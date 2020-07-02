dee24f97ec212ea057648fd1229059c0f497efe8
sudo-mechE <60611623+sudo-mechE@users.noreply.github.com>
2020-07-02 15:55:10 +0300

    Update YouTube Livestream Botter.py

--- YouTube Livestream Botter.py
+++ YouTube Livestream Botter.py
@@ -66,12 +66,11 @@ class proxy():
                 for url in urls:
                     data += requests.get(url).text
                     self.splited += data.split("\r\n") #scraping and splitting proxies
-            time.sleep(600)
+            time.sleep(6000)
     
     def get_proxy(self):
-            for i in range(2500):
-            	random1 = self.splited[i]
-            	return random1
+        random1 = random.choice(self.splited) #choose a random proxie
+        return random1
     def FormatProxy(self):
 	    proxyOutput = {'https' :'https://'+self.get_proxy()}
 	    return proxyOutput
@@ -87,13 +86,13 @@ def bot():
         try:
             s = requests.session()
 
-            resp = s.get("https://m.youtube.com/watch?v=" + token,headers={'Host': 'm.youtube.com', 'Proxy-Connection': 'keep-alive', 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7', 'Accept-Encoding': 'gzip, deflate'},proxies=proxy1.FormatProxy())   # simple get request to youtube for the base URL
+            resp = s.get("https://m.youtube.com/watch?v=" + token,headers={'Host': 'm.youtube.com', 'Proxy-Connection': 'keep-alive', 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Mobile Safari/537.36', 'Accept': 'image/webp,image/apng,image/*,*/*;q=0.8', 'Accept-Language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7', 'gzip, deflate, br'},proxies=proxy1.FormatProxy())   # simple get request to youtube for the base URL
             url = resp.text.split(r'videostatsWatchtimeUrl\":{\"baseUrl\":\"')[1].split(r'\"}')[0].replace(r"\\u0026",r"&").replace('%2C',",").replace("\/","/")  #getting the base url for parsing
             cl = url.split("cl=")[1].split("&")[0] #parsing some infos for the URL 
             ei = url.split("ei=")[1].split("&")[0]
             of = url.split("of=")[1].split("&")[0]
             vm = url.split("vm=")[1].split("&")[0]
-            s.get("https://s.youtube.com/api/stats/watchtime?ns=yt&el=detailpage&cpn=isWmmj2C9Y2vULKF&docid=" + token + "&ver=2&cmt=7334&ei=" + ei + "&fmt=133&fs=0&rt=1003&of=" + of +"&euri&lact=4418&live=dvr&cl=" + cl + "&state=playing&vm=" + vm + "&volume=100&c=MWEB&cver=2.20200313.03.00&cplayer=UNIPLAYER&cbrand=apple&cbr=Safari%20Mobile&cbrver=12.1.15E148&cmodel=iphone&cos=iPhone&cosver=12_2&cplatform=MOBILE&delay=5&hl=ru&cr=GB&rtn=1303&afmt=140&lio=1556394045.182&idpj=&ldpj=&rti=1003&muted=0&st=7334&et=7634",headers={'Host': 's.youtube.com', 'Proxy-Connection': 'keep-alive', 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1', 'Accept': 'image/png,image/svg+xml,image/*;q=0.8,video/*;q=0.8,*/*;q=0.5', 'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3', 'Referer': 'https://m.youtube.com/watch?v=' + token},proxies=proxy1.FormatProxy())   # API GET request
+            s.get("https://s.youtube.com/api/stats/watchtime?ns=yt&el=detailpage&cpn=isWmmj2C9Y2vULKF&docid=" + token + "&ver=2&referrer=https%3A%2F%2Fwww.google.com%2F&cmt=1713852.002&ei" + ei + "&fmt=134&fs=0&rt=6.002&of=" + of +"&euri&lact=7118&live=dvr&cl=" + cl + "&state=playing&vm=" + vm + "&fmt=134&fs=0&rt=6.002&of=0X62zLANLtGyuw1GbRs4Dw&euri&lact=7118&live=dvr&cl=319138800&osid=AAAAAKrvpsM%3AAOeUNAbH_tfBMZw8r2S1htO3RJ243Qwv8A&state=playing&vm=CAEQARgEKiBsUmpoTXRxc1czUTVHZ2RJbmktOXBNdnY3X3JnV3ItNjoyQUdiNlo4T1dibmt4eEgybHlnRXZ4Wk1CeTJ3dUhXcUdFUXBGeXdaQm1mdV9hT1lETHc&volume=100&cbrand=google&cbr=Chrome%20Mobile&cbrver=83.0.4103.116&c=MWEB&cver=2.20200701.03.01&cplayer=UNIPLAYER&cmodel=nexus%205&cos=Android&cosver=6.0&cplatform=MOBILE&delay=5&hl=en_US&cr=TR&uga=m30&rtn=16&afmt=140&idpj=-8&ldpj=-27&rti=6&muted=0&st=1713849.472&et=1713852.002",headers={'Host': 's.youtube.com', 'Proxy-Connection': 'keep-alive', 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Mobile Safari/537.36', 'Accept': 'image/webp,image/apng,image/*,*/*;q=0.8,video/*;q=0.8,*/*;q=0.5', 'Accept-Language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7', 'Referer': 'https://m.youtube.com/watch?v=' + token},proxies=proxy1.FormatProxy())   # API GET request
             
             
             a.botted += 1
@@ -112,4 +111,3 @@ while num < maxthreads :
 
 
 threading.Thread(target=bot).start()
-
